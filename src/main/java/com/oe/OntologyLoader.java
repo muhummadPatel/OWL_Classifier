fad7ef32d64e6c2400e0405edf9d4701e85a9356
Brian Mc George <Brian-McG.github@outlook.com>
2016-03-24 17:07:59 +0200

    Add OntologyLoader

--- src/main/java/com/oe/OntologyLoader.java
+++ src/main/java/com/oe/OntologyLoader.java
@@ -1,5 +1,62 @@
 package com.oe;
 
-public class OntologyLoader{
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.util.DLExpressivityChecker;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Class is responsible for loading ontologies from file
+ */
+public class OntologyLoader{
+    /**
+     * Takes a path to a file and a boolean representing if imports should be included and returns a set of OWLOntology objects
+     * @param filePath path to file being loaded
+     * @param isLoadingImports should imports be included in the set
+     * @return A set of OWLOntology objects
+     */
+    public static Set<OWLOntology> loadOntology(String filePath, boolean isLoadingImports) {
+        String extra;
+        if(isLoadingImports) {
+            extra = " including imports";
+        } else {
+            extra = " without imports";
+        }
+
+        System.out.println("Attempting to load " + filePath + extra);
+        OWLOntologyManager owlOntologyManager = OWLManager.createOWLOntologyManager();
+        OWLOntology ontology;
+        Set ontologies = new HashSet<OWLOntology>();
+
+        try{
+            File ontFile = new File(filePath);
+            ontology = owlOntologyManager.loadOntologyFromOntologyDocument(ontFile);
+            ontologies.add(ontology);
+            if(isLoadingImports) {
+                addImports(ontologies, ontology);
+            }
+
+
+            System.out.println(ontFile.getName() + " loaded" + extra);
+
+        }catch(org.semanticweb.owlapi.model.OWLOntologyCreationException ex){
+            System.out.println("Ontology creation Error:\n" + ex.getMessage());
+            System.exit(1);
+        }
+
+
+        return ontologies;
+    }
+
+    private static void addImports(Set<OWLOntology> ontologies, OWLOntology ontology) {
+        Set<OWLOntology> importOntologies = ontology.getImports();
+        for(OWLOntology singleImport : importOntologies) {
+            ontologies.add(singleImport);
+            addImports(ontologies, singleImport);
+        }
+    }
 }
